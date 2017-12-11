c216cd21feae7ec0d03644b6d3134771fd8697d4
Anil <modest.anil@gmail.com>
2017-12-11 13:05:41 +0800

    refactored

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import featureRanker
 import pandas as pd
 
 def main():
-    stock = pd.read_csv("testData.csv", nrows=20)
-    fRanker = featureRanker.FeatureRanker(['market_Cap','P_E_Ratio','Price','ROE','Dividend','Beta','Perf_YTD'], stock, 'Beta')
+    #stock = pd.read_csv("testData.csv", nrows=20)
+    #fRanker = featureRanker.FeatureRanker(['market_Cap', 'P_E_Ratio', 'Price', 'ROE', 'Dividend', 'Beta', 'Perf_YTD'],
+                                          #stock, 'Price')
+    stock = pd.read_csv("AAPL_quarterly_financial_data.csv")
+    fRanker = featureRanker.FeatureRanker(['Dividend per share', 'Price', 'P/E ratio', 'ROE', 'Revenue', 'Shareholders equity', 'Liabilities', 'Assets'],
+                                          stock, 'P/E ratio')
     ranks = fRanker.calculatefeatureranks()
 
 if __name__ == "__main__":
